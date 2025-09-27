32737ee4ab1532013c4107677a8d7a3eb5612e18
Rakshit <rakshit.gupta229@gmail.com>
2025-09-27 10:50:26 +0530

    Backend Deployed link added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mini-financial-trading-app.onrender.com/api",
 });
 
 // If token is stored in localStorage, send it automatically
